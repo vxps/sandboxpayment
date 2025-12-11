ab0cfc45bcb23bdbf9579ed34803dcf56a94ed98
Kirill <maksimovich.k@notissimus.com>
2025-12-11 18:10:55 +0300

    https fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     storeId: "dd420eef-c2db-4ee6-8a5d-2b865129cc83", 
-    apiUrl: "https://https://split.notissimus.com/api/order/create", 
+    apiUrl: "https://split.notissimus.com/api/order/create", 
 };
 
 document.addEventListener("DOMContentLoaded", () => {
